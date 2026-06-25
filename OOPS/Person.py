64193b00807ab35cb1755ddd99181af7c0818889
Prince Software <kumarprince95596186@gmail.com>
2026-06-25 09:28:37 +0530

    OOPS Concepts

--- OOPS/Person.py
+++ OOPS/Person.py
@@ -1,13 +1,14 @@
 class Person:
 
-    def __init__(self,name,email,mobile):
+    def __init__(self,name, mobile,address):
 
         self.name=name
-        self.email=email
         self.mobile=mobile
+        self.address=address
 
-p1=Person("Piyush","piyush34@gmail.com",9559890988)
+
+p1=Person("Prince Kumar",9559618602,"Bhadohi")
 
 for key,value in p1.__dict__.items():
+
     print(key, ":", value)
-         
